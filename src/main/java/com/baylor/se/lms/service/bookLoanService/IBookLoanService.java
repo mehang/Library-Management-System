b27ecc2bc1eb67f0559d4976aa09312b7223cee0
sanjel <arun_sanjel1@baylor.edu>
2019-10-24 21:17:58 -0500

    BookLoan Service with CRUD added

---
+++ src/main/java/com/baylor/se/lms/service/bookLoanService/IBookLoanService.java
@@ -0,0 +1,13 @@
+package com.baylor.se.lms.service.bookLoanService;
+
+import com.baylor.se.lms.model.Book;
+import com.baylor.se.lms.model.BookLoan;
+
+import java.util.List;
+
+public interface IBookLoanService {
+    public BookLoan saveBookLoan(BookLoan bookLoan);
+    public BookLoan getBookLoan(Long id);
+    public List<BookLoan> getBooks();
+    public void updateBook(BookLoan bookLoan);
+}
