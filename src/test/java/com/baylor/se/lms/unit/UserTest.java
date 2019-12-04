02c74d6222ee5d10b83875002ab2e63ac56631d0
Arun Sanjel <arun_sanjel1@baylor.edu>
2019-12-04 15:56:43 -0600

    Test for user model completed

--- src/test/java/com/baylor/se/lms/unit/UserTest.java
+++ src/test/java/com/baylor/se/lms/unit/UserTest.java
@@ -1,25 +1,19 @@
 package com.baylor.se.lms.unit;
 
 import com.baylor.se.lms.data.LibrarianRepository;
-import com.baylor.se.lms.dto.UserDTO;
 import com.baylor.se.lms.model.Librarian;
-import com.baylor.se.lms.model.User;
-import com.baylor.se.lms.service.impl.LibrarianService;
 import com.baylor.se.lms.service.impl.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 @SpringBootTest
@@ -32,10 +26,6 @@ public class UserTest {
 
     @Autowired
     private LibrarianRepository librarianRepository;
-    @Test
-    public void  testUserWithSameEmail(){
-
-    }
 
     @Test(expected = DataIntegrityViolationException.class)
     public void testLibrarianNotUniqueNumber(){
@@ -51,16 +41,54 @@ public class UserTest {
         if (librarianList.contains(librarian)) fail("Not Here");
 
     }
-    @Test(expected = Exception.class)
-    public void testTestLibrarianUniqueEmail(){
+    @Test(expected = DataIntegrityViolationException.class)
+    public void testLibrarianNoUniqueEmail(){
         Librarian librarian = new Librarian();
         librarian.setUsername("Arun");
         librarian.setPassword("hello");
-        librarian.setEmail("asanjel@mtu.edu"); // Not Uniquq
+        librarian.setEmail("asanjel@mtu.edu"); // Not Unique
         librarian.setName("Arun Sanjel");
         librarian.setPhoneNumber("9063693028");
+        librarianRepository.save(librarian);
         List<Librarian> librarianList = librarianRepository.findAllByDeleteFlagFalse();
-        if (librarianList.contains(librarian)) fail("Not Here");
+        if (librarianList.contains(librarian)) fail("Should not be saved");
+    }
 
+    @Test(expected = DataIntegrityViolationException.class)
+    public void testLibrarianNoUniqueUsername(){
+        Librarian librarian = new Librarian();
+        librarian.setUsername("asanjel"); // Not Unique
+        librarian.setPassword("hello");
+        librarian.setEmail("rai_ji@mtu.edu");
+        librarian.setName("Arun Sanjel");
+        librarian.setPhoneNumber("1233693028");
+        librarianRepository.save(librarian);
+        List<Librarian> librarianList = librarianRepository.findAllByDeleteFlagFalse();
+        if (librarianList.contains(librarian)) fail("Should not be saved");
+    }
+
+    @Test(expected = ConstraintViolationException.class)
+    public void testLibrarianInvalidPhoneNumber(){
+        Librarian librarian = new Librarian();
+        librarian.setUsername("rai_ji");
+        librarian.setPassword("hello");
+        librarian.setEmail("arun@mtu.edu");
+        librarian.setName("Arun Sanjel");
+        librarian.setPhoneNumber("90636930"); // Invalid Number
+        librarianRepository.save(librarian);
+        List<Librarian> librarianList = librarianRepository.findAllByDeleteFlagFalse();
+        if (librarianList.contains(librarian)) fail("Should not be saved");
+    }
+    @Test(expected = ConstraintViolationException.class)
+    public void testLibrarianInvalidEmail(){
+        Librarian librarian = new Librarian();
+        librarian.setUsername("rai_ji");
+        librarian.setPassword("hello");
+        librarian.setEmail("arun");
+        librarian.setName("Arun Sanjel");
+        librarian.setPhoneNumber("90636930"); // Invalid Number
+        librarianRepository.save(librarian);
+        List<Librarian> librarianList = librarianRepository.findAllByDeleteFlagFalse();
+        if (librarianList.contains(librarian)) fail("Should not be saved");
     }
 }
